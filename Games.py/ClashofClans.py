78a06ffef53b21f37af7b489ebd2f63a5613342e
Bora Saglam <posarobit@gmail.com>
2025-06-11 21:10:04 +0300

    Games.py değişiklik yaptım.

--- Games.py/ClashofClans.py
+++ Games.py/ClashofClans.py
@@ -1,8 +1,8 @@
 print(" 1. Savuma Binaları")
 
-print("Top" \
+print("Top: " \
 "İlk savunma binalarıdır. Geliştirmek için ana binada en az 7, inşaatçı binasında en az 4 seviye olması gerekir. Geliştirildiğinde mesafesi kısalır ama çift toplar atar. Hedef türü karadır. Hasar türü tek hedeftir. En yüksek 21 seviyedir.")
-print("Okçu Kulesi " \
+print("Okçu Kulesi: " \
 "Belediye Binası 2. seviyeye yükseltildiğinde kilidi açılır. Toplara göre menzili uzundur. Geliştirmek için ana binada en az 10, inşaatçı binasında en az 6 seviye olması gerekir. Geliştirildiğinde mesafesi kısalır ama 2 kat hasar vurur. Hedef türü kara ve havadır. Hasar türü tek hedeftir. En yüksek 21 seviyedir.")
 print("Havan: " \
 "Belediye Binası 3. seviyeye yükseltildiğinde kilidi açılır. Düşük canlı olan birliklere karşı etkilidir. Çok yakınındaki birliklere hasar veremez. Geliştirmek için ana binada ve inşaatçı binasında en az 8 seviye olması gerekir. Geliştirildiğinde vurma hızı azalır ama aynı anda 3 top fırlatır hale gelir. Hedef türü karadır. Hasar türü alan tesiridir. En yüksek 15 seviyedir.")
@@ -16,7 +16,7 @@ print("Gizli Tesla: " \
 "Belediye Binası 7. seviyeye yükseltildiğinde kilidi açılır. Düşman kendisine yaklaşmadığında veya köyün %51 yıkılmadığında gözükmeyen savunma binalarıdır. Ortaya çıktığında elektrik ile hasar verir. Hedef türü kara ve havadır. Hasar türü tek hedeftir. En yüksek 13 seviyedir.")
 print("Bomba Kulesi:" \
 " Belediye Binası 8. seviyeye yükseltildiğinde kilidi açılır. Bir iskeletin bomba atmasıyla hasarı veren savunma binalarıdır. Yıkılınca büyük bir alan hasarı verir. Hedef türü kara ve havadır. Hasar türü alan tesiridir. En yüksek 10 seviyedir.")
-print("X-Yayı:" \
+print("X-Yayı: " \
 " Belediye Binası 9. seviyeye yükseltildiğinde kilidi açılır. Düşük hasarlı ama en hızlı savunma binalarıdır. Hedef türü kara olarak da kara ve hava olarak da ayarlanabilir ancak kara ve hava olarak ayarlanırsa menzili daha kısa olur. Hasar türü tek hedeftir. En yüksek 10 seviyedir.")
 print("Cehennem Kulesi: " \
 "Belediye Binası 10. seviyeye yükseltildiğinde kilidi açılır. Alev püskürten savunma binalarıdır. Tek hedef modunda aynı hedefe ne kadar çok odaklanırsa verdiği hasar katlanarak artar. Hedef türü kara ve havadır. Hasar türü çok hedef olarak da tek hedef olarak da ayarlanabilir ancak tek hedef olarak ayarlanırsa menzili daha kısa olur. En yüksek 9 seviyedir.")
