c0f69193979a5165b3fc69d608c3ef3e64e2b389
Bora Saglam <posarobit@gmail.com>
2025-06-11 22:13:13 +0300

    Ulkeler-Sehirler.py değişiklik yaptım.

--- Ulkeler-Sehirler.py
+++ Ulkeler-Sehirler.py
@@ -4,10 +4,10 @@ print("İngiltere")
 
 print("Büyük-küçük şehirler, köyler ve turistik yerler")
 
-print("Londra" \
-"NewCastle" \
-"Machester" \
-"Liverpool" \
-"Brighton" \
-"Bournmounth" \
-"Leeds")
+print("Londra")
+print("NewCastle")
+print("Machester")
+print("Liverpool")
+print("Brighton")
+print("Bournmounth")
+print("Leeds")
