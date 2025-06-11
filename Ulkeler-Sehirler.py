cf01be63cc1a90bede013b0112469e66f38b377f
Bora Saglam <posarobit@gmail.com>
2025-06-11 22:11:04 +0300

    Ulkeler-Sehirler.py ekledim.

---
+++ Ulkeler-Sehirler.py
@@ -0,0 +1,13 @@
+print("Ülke")
+
+print("İngiltere")
+
+print("Büyük-küçük şehirler, köyler ve turistik yerler")
+
+print("Londra" \
+"NewCastle" \
+"Machester" \
+"Liverpool" \
+"Brighton" \
+"Bournmounth" \
+"Leeds")
