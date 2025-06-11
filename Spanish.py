53b1bbeb797e6ffa9c18c94bcf69d0302c81a62b
Bora Saglam <posarobit@gmail.com>
2025-06-11 15:49:57 +0300

    Spanish.py ekledim

---
+++ Spanish.py
@@ -0,0 +1,16 @@
+import keyboard
+mesaj = "Karşılık gelen rakamları giriniz:"
+uno = input(mesaj)
+dos = input(mesaj)
+tres = input(mesaj)
+cuatro = input(mesaj)
+cinco = input(mesaj)
+seis = input(mesaj)
+siete = input(mesaj)
+ocho = input(mesaj)
+nueve = input(mesaj)
+diez = input(mesaj)
+print("")
+print("Cevapları görmek için enter' a basınız.")
+keyboard.wait("enter")
+uno = "bir"
