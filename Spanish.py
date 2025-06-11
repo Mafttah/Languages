7619ee0284987c9008dda3f3ff070bde84fc86d6
Bora Saglam <posarobit@gmail.com>
2025-06-11 16:03:09 +0300

    Spanish.py değişiklik yaptım

--- Spanish.py
+++ Spanish.py
@@ -3,10 +3,6 @@ print = input("uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, diez")
 
 mesaj = "Karşılık gelen rakamları giriniz:"
 
-
-print("-- enter")
-keyboard.wait("enter")
-
 uno = input(mesaj)
 dos = input(mesaj)
 tres = input(mesaj)
@@ -17,10 +13,6 @@ siete = input(mesaj)
 ocho = input(mesaj)
 nueve = input(mesaj)
 diez = input(mesaj)
-print("")
 
-print("Cevapları görmek için enter' a tıklayınız")
-keyboard.wait("enter")
-print("")
 
 print("uno = 1, dos = 2, tres = 3, cuatro = 4, cinco = 5, seis = 6, siete = 7, ocho = 8, nueve = 9, diez = 10")
