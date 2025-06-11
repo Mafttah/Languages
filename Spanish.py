871fadfacddee34a400b05c241652a1fc6b10483
Bora Saglam <posarobit@gmail.com>
2025-06-11 16:47:23 +0300

    Spanish.py değişiklik yaptım

--- Spanish.py
+++ Spanish.py
@@ -1,5 +1,12 @@
+import keyboard
 print = input("uno = ?, dos = ?, tres = ?, cuatro = ?, cinco = ?, seis = ?, siete = ?, ocho = ?, nueve = ?, diez = ?")
+print("Devam etmek için spacebara basınız.")
+keyboard.wait("space")
+
 mesaj = "Karşılık gelen rakamları giriniz:"
+
+print("Devam etmek için spacebara basınız.")
+keyboard.wait("space")
 
 uno = input(mesaj)
 dos = input(mesaj)
@@ -7,10 +14,12 @@ tres = input(mesaj)
 cuatro = input(mesaj)
 cinco = input(mesaj)
 seis = input(mesaj)
-
 siete = input(mesaj)
 ocho = input(mesaj)
 nueve = input(mesaj)
 diez = input(mesaj)
+
+print("Devam etmek için spacebara basınız.")
+keyboard.wait("space")
 
 print = input("uno = 1, dos = 2, tres = 3, cuatro = 4, cinco = 5, seis = 6, siete = 7, ocho = 8, nueve = 9, diez = 10")
