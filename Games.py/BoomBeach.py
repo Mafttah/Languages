53adda73bc78aae2ed8f5516cd4ce07376fb83c1
Bora Saglam <posarobit@gmail.com>
2025-06-11 21:22:36 +0300

    BoomBeach.py ekledim.

---
+++ Games.py/BoomBeach.py
@@ -0,0 +1,18 @@
+print(" 2. Savunma Binaları")
+
+print("Keskin Nişancı Kulesi: " \
+"Oyunda ilk inşa edilen savunma binasıdır. Tek bir birime yüksek hasar verir. Clash of Clans'taki okçu kulesine benzer. En fazla 23. seviyeye yükseltilebilir.")
+print("Havan Topu: " \
+"Orta menzilli bir binadır. Alan hasarı veren toplar atar. Bununla birlikte kör noktası vardır, yakınına vuramaz. Clash of Clans'taki havana benzer. Oyunun 2. savunmasıdır. En fazla 24. seviyeye yükseltilebilir.")
+print("Makineli Tüfek: " \
+"Orta menzillidir. Aynı anda birkaç birliğe hasar verir. Oyunun 3. savunmasıdır. En fazla 23. seviyeye yükseltilebilir.")
+print("Top: " \
+"Orta menzillidir. Tek bir birime çok yüksek hasar verir. Bu da onları ağır ve zırhlı düşmanlara karşı ideal bir silah yapar fakat bu silah, kalabalık piyade gruplarına karşı o kadar etkili değildir. Tanklara iki kat hasar verir.Oyunun 4. savunmasıdır. Clash of Clans'taki topa benzer. En fazla 23. seviyeye yükseltilebilir.")
+print("Alev Silahı: " \
+"Çok büyük miktarda hasar verir. Kısa menzillidir. Vurduğu birimler bir süre daha yanmaya devam eder. Oyundaki 5. savunmadır. En fazla 21. seviyeye yükseltilebilir.")
+print("Boom Topu: " \
+"Oldukça yıkıcı hasarlar veren fakat doldurulması uzun süren bir silahtır. Canı yüksek olan birliklere karşı koymak için idealdir. Tanklara iki kat hasar verir. Oyunun 6. savunmasıdır. En fazla 17. seviyeye yükseltilebilir.")
+print("Roketatar: " \
+"5 saniyede 6 roket ateşleyebilen, askeri birimler için oldukça tehlikeli uzun menzilli savunma birimidir. Uzun mesafeden düşmana saldırabilir, ama yakın mesafeden düşmana saldıramaz, aynı Havan Topu gibi kör noktası vardır. Oyundaki 6. savunmadır. En fazla 15. seviyeye yükseltilebilir.")
+print("Şok Atar: " \
+"Düşman birliklerini hem kısa süreliğine donduran hem onlara zarar veren bir binadır. Oyundaki son savunma binasıdır. En fazla 10. seviye olabilir.")
