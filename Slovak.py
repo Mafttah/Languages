3168afc43a2f8b8e2773196de822a57573d6e8c3
Bora Saglam <posarobit@gmail.com>
2025-06-12 10:41:46 +0300

    Slovak.py değişiklik yaptım.

--- Slovak.py
+++ Slovak.py
@@ -1,2 +1,31 @@
-print("Slovak")
-print = "jeden, dva, tri, štyri, päť, šesť, sedem, osem, deväť, desať"
+import keyboard
+
+print("1'den 10' a Sayılar")
+cevap = input("jeden =?, dva =?, tri =?, styri =?, pat =?, sest =?, sedem =?, osem =?, devat =?, desat =?")
+print("")
+print("Devam etmek için enter'a basınız.")
+keyboard.wait("enter")
+
+mesaj = "Karşılık gelen rakamları giriniz:"
+
+print("Devam etmek için enter'a basınız.")
+keyboard.wait("enter")
+
+jeden = input(mesaj)
+dva = input(mesaj)
+tri = input(mesaj)
+styri = input(mesaj)
+pat = input(mesaj)
+sest = input(mesaj)
+sedem = input(mesaj)
+osem = input(mesaj)
+devat = input(mesaj)
+desat = input(mesaj)
+
+print("Devam etmek için spacebar'a basınız.")
+keyboard.wait("space")
+print("")
+
+print = input("jeden = 1, dva = 2, tri = 3, styri = 4, pat = 5, sest = 6, sedem = 7, osem = 8, devat = 9, desat = 10")
+
+print("Verbs")
