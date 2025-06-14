153cfb9c5429462e7b6b97473256505baccee9b0
Bora Saglam <posarobit@gmail.com>
2025-06-14 23:10:59 +0300

    Spanish.py değişiklik yaptım.

--- Spanish.py
+++ Spanish.py
@@ -3,52 +3,174 @@ import keyboard
 import colorama
 from colorama import Fore
 
-print(Fore.RED,"1'den 10' a Sayılar")
-print(Fore.WHITE,"uno =?, dos =?, tres =?, cuatro =?, cinco =?, seis =?, siete =?, ocho =?, nueve =?, diez =?")
-print("")
+#Lesson_1 = "Numbers"
+
+#print(Fore.RED,"Numbers from 1 to 10")
+#print(Fore.WHITE,"uno =?, dos =?, tres =?, cuatro =?, cinco =?, seis =?, siete =?, ocho =?, nueve =?, diez =?")
+#print("")
 
 
-mesaj = "uno: "
-uno    = input(mesaj)
-mesaj = "dos: "
-dos    = input(mesaj)
-mesaj = "tres: "
-tres   = input(mesaj)
-mesaj = "cuatro: "
-cuatro = input(mesaj)
-mesaj = "cinco: "
-cinco  = input(mesaj)
-mesaj = "seis: "
-seis   = input(mesaj)
-mesaj = "siete: "
-siete  = input(mesaj)
-mesaj = "ocho: "
-ocho   = input(mesaj)
-mesaj = "nueve: "
-nueve  = input(mesaj)
-mesaj = "diez: "
-diez   = input(mesaj)
+#mesaj = "uno: "
+#uno    = input(mesaj)
+#mesaj = "dos: "
+#dos    = input(mesaj)
+#mesaj = "tres: "
+#tres   = input(mesaj)
+#mesaj = "cuatro: "
+#cuatro = input(mesaj)
+#cinco  = input(mesaj)
+##mesaj = "cinco: "
+#mesaj = "seis: "
+#seis   = input(mesaj)
+#mesaj = "siete: "
+#siete  = input(mesaj)
+##mesaj = "ocho: "
+#ocho   = input(mesaj)
+#mesaj = "nueve: "
+#nueve  = input(mesaj)
+#mesaj = "diez: "
+#diez   = input(mesaj)
 
-print("Devam etmek için spacebar'a basınız.")
-keyboard.wait("space")
+#print("Devam etmek için spacebar'a basınız.")
+#keyboard.wait("space")
 
-print(f"\nGirilen Değerler: \nuno = {uno}, dos = {dos}, tres = {tres}, cuatro = {cuatro}, cinco = {cinco}, seis = {seis}, siete = {siete}, ocho = {ocho}, nueve = {nueve}, diez = {diez}")
+#print(f"\nGirilen Değerler: \nuno = {uno}, dos = {dos}, tres = {tres}, cuatro = {cuatro}, cinco = {cinco}, seis = {seis}, siete = {siete}, ocho = {ocho}, nueve = {nueve}, diez = {diez}")
 
-print("\nCevap Anahtarı:\n")
-print("uno = 1\n, dos = 2\n, tres = 3\n, cuatro = 4\n, cinco = 5\n, seis = 6\n, siete = 7\n, ocho = 8\n, nueve = 9\n, diez = 10\n")
-print("İşlemler")
+#print("\nCevap Anahtarı:\n")
+#print("uno = 1\n, dos = 2\n, tres = 3\n, cuatro = 4\n, cinco = 5\n, seis = 6\n, siete = 7\n, ocho = 8\n, nueve = 9\n, diez = 10\n")
+#print("İşlemler")
 
-sayi1 = int(input("Birinci sayiyi giriniz: "))
-sayi2 = int(input("İkinci sayiyi giriniz: "))
-sayi3 = int(input("Üçüncü Sayiyi giriniz: "))
+#sayi1 = int(input("Birinci sayiyi giriniz: "))
+#sayi2 = int(input("İkinci sayiyi giriniz: "))
+#sayi3 = int(input("Üçüncü Sayiyi giriniz: "))
 
-toplam = sayi1 + sayi2 + sayi3
-carpma = sayi1 * sayi2 * sayi3
-cikarma = sayi1 - sayi2 - sayi3
-bolme = sayi1 / sayi2
-islem = (sayi1 * sayi2 ) - sayi2 * sayi3 / sayi1
-print(toplam) 
-print(carpma) 
-print(cikarma) 
-print(bolme) 
-print(islem)
+#toplam = sayi1 + sayi2 + sayi3
+#carpma = sayi1 * sayi2 * sayi3
+#cikarma = sayi1 - sayi2 - sayi3
+#bolme = sayi1 / sayi2
+#islem = (sayi1 * sayi2 ) - sayi2 * sayi3 / sayi1
+#print(toplam) 
+#print(carpma) 
+#print(cikarma) 
+#print(bolme) 
+#print(islem)
+
+print("Greeting")
+
+print("Hola = Hello")
+print("Como_Estas = How are you?")
+print("Mucho_gusto = Nice to meet you")
+print("Hasta_manana = See you tomorrow")
+print("La_leche = the milk")
+print("La_maleta = suitcase")
+print("El_pan = the bread")
+print("La_mujer = the woman")
+print("El_azucar = the sugar")
+print("El_dinero = the money")
+
+print("Regular verbs are divided into 3 categories")
+
+print(Fore.RED, "1. -Ar Verbs")
+print(Fore.WHITE,"pasar = to pass, to spend (time), to happen\n,quedar = to stay, remain")
+print("hablar = to speak")
+print("llevar = to carry, bring")	
+print("dejar = to leave, abandon, to let, allow")	
+print("llamar = to call, to name")
+print("tomar = to take, drink")
+print("tratar = to treat, handle")	
+print("mirar = to watch, look at")	
+print("esperar = to wait for, to hope")	
+print("entrar =	to enter, go in, come in")	
+print("trabajar = to work")
+print("terminar = to finish, end")
+print("necesitar = to need, require")
+print("resultar = to turn out (to be)")
+print("cambiar = to change")
+print("presentar = to introduce")	
+print("crear = to create, to make")	
+print("considerar = to consider")
+print("acabar = to finish, end")
+print("ganar = to win, gain, earn, get, acquire")	
+print("formar = to form, shape, fashion, make")
+print("aceptar = to accept, approve, to agree to")	
+print("lograr = to get, obtain, to achieve, attain")		
+print("preguntar = to ask, inquire")		
+print("estudiar = to study")
+print("ayudar = to help")
+print("gustar = to please, be pleasing")		
+print("escuchar = to listen, hear")
+print("levantar = to raise, to lift")
+print("intentar = to try, attempt")
+print("usar = to use")
+print("olvidar = to forget")
+print("ocuoar = to occupy")
+print("fijar = to fix, fasten, secure")
+print("comprar = to buy, purchase")
+print("evitar = to avoid, to prevent")
+print("interesar = to interest, be of interest (to)")	
+print("echar = to throw, cast, fling")
+print("importar = to import, to be important")	
+print("observar = to observe")
+print("imaginar = to imagine")	
+print("desarrollar = to develop, expand, to unroll, unwind, to unfold")	
+print("señalar = to point out, indicate, to signal")
+print("preparar = to prepare, get (something) ready")
+print("faltar = to lack, be lacking, be missing")
+print("acompañar = to accompany")
+print("desear = to desire, want, wish")	
+print("enseñar = to teach, instruct, train, educate")
+print("representar = to represent")
+print("mandar = to order (give an order), to send")	
+print("asegurar = to assure, secure, insure")
+print("matar = to kill, slaughter")
+print("guardar = to guard, protect, to keep")	
+print("iniciar = to initiate, begin, start")
+print("bajar = to lower, go down, descend, download")	
+print("notar = to note, notice, observe")
+print("cortar = to cut")
+print("aprovechar = to take advantage of")	
+print("apoyar = to support, hold up, to back")	
+print("aumentar = to increase, add to, rise")
+print("abandonar = to abandon, leave behind, desert, to quit, give up")	
+print("quitar = to remove, take away")
+print("conservar = to preserve, conserve, to keep, retain")
+print("funcionar = to function, to run")
+print("anunciar = to announce")
+print("comentar = to comment on")	
+print("participar = to participate, to share in, to inform")	
+print("escapar = to escape")
+print("tirar = to throw, to shoot, to throw away, to pull")	
+print("contestar = to answer")
+print("preocupar = to worry")
+print("prestar = to lend")
+print("pesar = to weigh, to weigh down")	
+print("viajar = to travel, journey")
+
+print(Fore.RED, "2. -Er Verbs")
+print(Fore.WHITE,"deber = to owe, must, should, ought to")
+print("comprender = to understand, comprehend")
+print("correr = to run")
+print("comer = to eat")
+print("suceder = to happen, to succeed, follow")	
+print("aprender = to learn")
+print("responder = to respond, answer, reply to")
+print("vender = to sell")
+print("meter = to put (in), place, insert")	
+print("pretender = to attempt")
+print("corresponder = to correspond with")
+print("depender = to depend")
+
+print(Fore.RED, "3. -Ir Verbs")
+print(Fore.WHITE,"vivir = to live")
+print("existir = to exist")	
+print("ocurrir = to occur, happen")	
+print("recibir = to receive, to welcome, greet")	
+print("permitir = to permit, allow")
+print("partir = to divide, to leave")
+print("cumplir = to fulfil, carry out")	
+print("decidir = to decide, settle, resolve")
+print("subir = to go up, rise, move up, climb, raise up")
+print("sufrir = to suffer, to undergo, experience")
+print("compartir = to share, to divide (up)")
+print("consistir = to consist")
+print("insistir = to insist")
